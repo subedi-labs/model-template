27042c3ee951b93f7c97c606224971e7721c5fdd
psubedi <subedipukar@gmail.com>
2026-02-15 13:15:47 -0500

    test

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/test")
-def test():
-    return {"message": "This is a test endpoint"}
-
 @app.get("/health")
 def health():
     return {"status": "ok"}
